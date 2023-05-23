8d47de82824db66cca8486cb6e2d3e76c719bd96
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-05-23 20:11:23 +1000

    Asynchronous (Non-Blocking) HTTP Calls
    This version of the program modifies the previous one to use Python async features. It also imports the aiohttp module, which is a library to make HTTP requests in an asynchronous fashion using asyncio.
    
    The tasks here have been modified to remove the yield call since the code to make the HTTP GET call is no longer blocking. It also performs a context switch back to the event loop.

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import queue
-import requests
+import asyncio
+import aiohttp
 from codetiming import Timer
 
 
-def task(name, work_queue):
+async def task(name, work_queue):
     timer = Timer(text=f"Task {name} elapsed time: {{:.1f}}")
-    with requests.Session() as session:
+    async with aiohttp.ClientSession() as session:
         while not work_queue.empty():
-            url = work_queue.get()
+            url = await work_queue.get()
             print(f"Task {name} getting URL: {url}")
             timer.start()
-            session.get(url)
+            async with session.get(url) as response:
+                await response.text()
             timer.stop()
-            yield
 
 
-def main():
+async def main():
     """
     This is the main entry point for the program
     """
     # Create the queue of work
-    work_queue = queue.Queue()
+    work_queue = asyncio.Queue()
 
     # Put some work in the queue
     for url in [
@@ -32,22 +32,15 @@ def main():
         "http://facebook.com",
         "http://twitter.com",
     ]:
-        work_queue.put(url)
-
-    tasks = [task("One", work_queue), task("Two", work_queue)]
+        await work_queue.put(url)
 
     # Run the tasks
-    done = False
     with Timer(text="\n Total elapsed time: {:.1f}"):
-        while not done:
-            for t in tasks:
-                try:
-                    next(t)
-                except StopIteration:
-                    tasks.remove(t)
-                if len(tasks) == 0:
-                    done = True
+        await asyncio.gather(
+            asyncio.create_task(task("One", work_queue)),
+            asyncio.create_task(task("Two", work_queue)),
+        )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

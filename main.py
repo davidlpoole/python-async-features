4d32ed33591de3e4b586fe530654d472a1e4a177
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-05-23 18:54:51 +1000

    Simple co-operative concurrency
    The next version of the program allows the two tasks to work together. Adding a yield statement means the loop will yield control at the specified point while still maintaining its context. This way, the yielding task can be restarted later.
    
    The yield statement turns task() into a generator. A generator function is called just like any other function in Python, but when the yield statement is executed, control is returned to the caller of the function. This is essentially a context switch, as control moves from the generator function to the caller.
    
    The interesting part is that control can be given back to the generator function by calling next() on the generator. This is a context switch back to the generator function, which picks up execution with all function variables that were defined before the yield still intact.

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import queue
 
 
-def task(name, work_queue):
-    if work_queue.empty():
-        print(f"Task {name} nothing to do")
-    else:
-        while not work_queue.empty():
-            count = work_queue.get()
-            total = 0
-            print(f"Task {name} running")
-            for x in range(count):
-                total += 1
-            print(f"Task {name} total: {total}")
+def task(name, queue):
+    while not queue.empty():
+        count = queue.get()
+        total = 0
+        print(f"Task {name} running [queue: {count}]")
+        for x in range(count):
+            total += 1
+            yield
+        print(f"Task {name} total: {total}")
 
 
 def main():
@@ -26,11 +24,18 @@ def main():
         work_queue.put(work)
 
     # Create some synchronous tasks
-    tasks = [(task, "One", work_queue), (task, "Two", work_queue)]
+    tasks = [task("One", work_queue), task("Two", work_queue)]
 
     # Run the tasks
-    for t, n, q in tasks:
-        t(n, q)
+    done = False
+    while not done:
+        for t in tasks:
+            try:
+                next(t)
+            except StopIteration:
+                tasks.remove(t)
+            if len(tasks) == 0:
+                done = True
 
 
 if __name__ == "__main__":

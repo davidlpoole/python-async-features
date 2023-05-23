ca319a320fde9e7672729cc0f1032f808cadd8ba
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-05-23 19:26:30 +1000

    Cooperative Concurrency With Non-Blocking Calls
    The next version of the program has been modified quite a bit. It makes use of Python async features using asyncio/await provided in Python 3.
    
    The time and queue modules have been replaced with the asyncio package. This gives your program access to asynchronous friendly (non-blocking) sleep and queue functionality.

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
-import time
-import queue
+import asyncio
 from codetiming import Timer
 
 
-def task(name, queue):
+async def task(name, work_queue):
     timer = Timer(text=f"Task {name} elapsed time: {{:.1f}}")
-    while not queue.empty():
-        delay = queue.get()
-        print(f"Task {name} running [delay: {delay}]")
+    while not work_queue.empty():
+        delay = await work_queue.get()
+        print(f"Task {name} running")
         timer.start()
-        time.sleep(delay)
+        await asyncio.sleep(delay)
         timer.stop()
-        yield
 
 
-def main():
+async def main():
     """
     This is the main entry point for the program
     """
     # Create the queue of work
-    work_queue = queue.Queue()
+    work_queue = asyncio.Queue()
 
     # Put some work in the queue
     for work in [15, 10, 5, 2]:
-        work_queue.put(work)
-
-    # Create some synchronous tasks
-    tasks = [task("One", work_queue), task("Two", work_queue)]
+        await work_queue.put(work)
 
     # Run the tasks
-    done = False
     with Timer(text="\nTotal elapsed time: {:.1f}"):
-        while not done:
-            for t in tasks:
-                try:
-                    next(t)
-                except StopIteration:
-                    tasks.remove(t)
-                if len(tasks) == 0:
-                    done = True
+        await asyncio.gather(
+            asyncio.create_task(task("One", work_queue)),
+            asyncio.create_task(task("Two", work_queue)),
+        )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
